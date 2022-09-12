94bee809485cea47065b474491e7f26f86a30f87
rohitgulam <rohitsgulam@gmail.com>
2022-09-12 13:42:30 +0300

    removed sudebar added individual routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Sidebar from './components/Sidebar';
 import AddCustomer from './views/AddCustomer';
 import Home from './views/Home';
-import { Routes, Route } from 'react-router-dom';
+import Customers from './views/Customers';
+import Login from './views/Login';
+import ProtectedRoutes from './components/ProtectedRoutes'
 
 function App() {
   return (
-    <div className="flex bg-slate-200">
-      <Sidebar />
+    <div className="bg-slate-200">
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/add-customer" element={ <AddCustomer /> } />
+        
+        <Route element={ <ProtectedRoutes/> } >
+          <Route path="/" element={ <Home /> } exact />
+          <Route path="/add-customer" element={ <AddCustomer /> } />
+          <Route path="/customers" element={ <Customers /> } />
+        </Route>
+        
+        <Route path="/login" element={ <Login /> } />
       </Routes>
     </div>
   );
