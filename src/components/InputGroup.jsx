e7ef13b984f1fec217083deabd8649728a2fcca3
rohitgulam <rohitsgulam@gmail.com>
2022-09-14 17:07:57 +0300

    :bug: added more functions (value, onchange)

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function InputGroup({ label, type, placeholder}) {
+function InputGroup({ label, type, placeholder, name, value, onChange}) {
   return (
-    <div className='flex flex-col w-full px-5'>
+    <div className='flex flex-col w-full'>
         <label
           className='text-xl'
-          htmlFor="fname">{label}</label>
-        <input className='border-2 rounded border-gray-600 h-11 text-xl px-2' type={type} name="fname" id="fname" placeholder={placeholder} required />
+          htmlFor={name}>{label}</label>
+        <input className='border-2 rounded border-gray-600 h-11 text-xl px-2 py-1' type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} required />
     </div>
   )
 }
