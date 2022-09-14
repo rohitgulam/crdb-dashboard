9c50176f1b9f7fd420cc6f8130be15aaeb6b0a9e
rohitgulam <rohitsgulam@gmail.com>
2022-09-14 17:10:02 +0300

    :bug: fixed overallping with sidebar

--- src/views/Customers.jsx
+++ src/views/Customers.jsx
@@ -18,9 +18,9 @@ function Customers() {
   }
 
   return (
-    <div className="flex">
+    <div className="flex h-full">
     <Sidebar />
-    <div className='flex flex-col my-20 w-[90vw] mx-12 px-10 py-4' >
+    <div className='flex flex-col rounded w-[90%] min-h-[84%] ml-[450px] mr-16 mt-20 py-10 px-24' >
         <div className='flex items-center justify-between py-4' >
             <h1 className='text-xl font-medium'>Customer List</h1>
             <Button 
@@ -32,7 +32,7 @@ function Customers() {
         <div className='h-full bg-white py-5 px-4' >
           <table className='w-full text-left table-auto' >
             <tr className='border-8 border-transparent' >
-              <th className='py-4'>Customer ID</th>
+              <th className='py-4'>Account Number</th>
               <th>Name</th>
               <th>Register Date</th>
               <th>Email</th>
@@ -41,7 +41,7 @@ function Customers() {
             </tr>
             {customers.map( (customer) => {
               return(
-                <tr className='border-8 border-transparent text-gray-700' >
+                <tr className='border-8 border-transparent text-gray-700'  >
                   <td className='py-4' >{customer.account_number}</td>
                   <td>{customer.name}</td>
                   <td>{customer.created_at}</td>
