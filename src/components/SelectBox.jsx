377479d17f7474dd91ff24972488a86fa4f9349e
Allen_A_M <allen22052000@gmail.com>
2022-09-14 10:50:41 +0300

    created select box component

---
+++ src/components/SelectBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SelectBox({label , name1, name2}) {
+  return (
+    <div className='flex flex-col w-full px-5'>
+         <label
+         className='text-xl'
+         htmlFor="name">{label}</label>
+    <select className='border-2 rounded border-gray-600 bg-white h-11 text-xl px-2' name="name" placeholder=" "  required>
+    <option className='hidden '></option>
+    <option value="name1">{name1}</option>
+    <option value="name2">{name2}</option>
+  </select>
+    </div>
+  )
+}
+
+export default SelectBox
