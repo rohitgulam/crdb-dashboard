53e79f0c6f1e64f2898cf334bf0096c06f1f5fc2
rohitgulam <rohitsgulam@gmail.com>
2022-09-17 17:35:37 +0300

    imported protected routes and auth context

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import AddCustomer from './views/AddCustomer';
 import Home from './views/Home';
 import Customers from './views/Customers';
 import Login from './views/Login';
-import ProtectedRoutes from './components/ProtectedRoutes'
+import ProtectedRoutes from './components/ProtectedRoutes';
+import AuthContext from './AuthContext';
+import { useContext } from 'react';
+
 
 function App() {
+
+  const {isAuth} = useContext(AuthContext);
+
+  // changeAuth();
+  // console.log("coming from App", isAuth);
+
   return (
     <div className="bg-slate-200 min-h-[100vh]">
-      <Routes>
-        
-        <Route element={ <ProtectedRoutes/> } >
-          <Route path="/" element={ <Home /> } exact />
-          <Route path="/add-customer" element={ <AddCustomer /> } />
-          <Route path="/customers" element={ <Customers /> } />
-        </Route>
-        
-        <Route path="/login" element={ <Login /> } />
-      </Routes>
+          <Routes>
+            `<Route element={ <ProtectedRoutes isAuth={isAuth} /> } >
+              <Route path="/" element={ <Home /> } exact />
+              <Route path="/add-customer" element={ <AddCustomer /> } />
+              <Route path="/customers" element={ <Customers /> } />
+            </Route>
+            
+            <Route path="/login" element={ <Login /> } />
+          </Routes>`
     </div>
   );
 }
